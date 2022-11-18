e92e6dd311caf1c62eeafeb455442de637effa5d
Tom Brulin <t.brulin@outlook.com>
2022-11-18 10:05:09 +0100

    Redirect on the first page when on root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Page() {
+import { redirect } from "next/navigation";
+import { DocumentationService } from "../services/documentation-service";
+
+export default async function Page() {
+    const documentationService = new DocumentationService();
+    const categories = await documentationService.getCategories();
+
+    if (categories.length > 0) {
+        return redirect(categories[0].getFirstPageLink());
+    }
+
     return (
         <>
-            <h1>Hello from app folder</h1>
         </>
     );
   }
